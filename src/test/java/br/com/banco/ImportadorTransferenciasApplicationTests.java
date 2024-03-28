c4a5deb3023915ff7ef73b425dc3b1abe1b5cb48
EduardoBabetto <edubabetto@gmail.com>
2024-03-28 16:27:23 -0300

    Criado classes controller e service

---
+++ src/test/java/br/com/banco/ImportadorTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportadorTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
