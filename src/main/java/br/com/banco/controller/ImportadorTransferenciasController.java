b5ac990f950bc5f33c8ca29f18dc71d293b1608e
EduardoBabetto <edubabetto@gmail.com>
2024-04-01 15:26:22 -0300

    Feita a geracao de transferencias

--- src/main/java/br/com/banco/controller/ImportadorTransferenciasController.java
+++ src/main/java/br/com/banco/controller/ImportadorTransferenciasController.java
@@ -1,5 +1,20 @@
 package br.com.banco.controller;
 
-public class ImportadorTransferenciasController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import br.com.banco.service.ImportadorTransferenciasService;
 
+@Controller
+@RequestMapping("/transferencia")
+public class ImportadorTransferenciasController {
+	ImportadorTransferenciasService its = new ImportadorTransferenciasService();
+	@PostMapping("/salvar/{i}")
+	public ResponseEntity<String> salvar (@PathVariable int i){
+		its.gerarTransferencia(i);
+		return new ResponseEntity<>("transferencias geradas",HttpStatus.OK);
+	}
 }
--- src/main/java/br/com/banco/models/Usuario.java
+++ src/main/java/br/com/banco/models/Usuario.java
@@ -1,5 +1,65 @@
 package br.com.banco.models;
 
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Usuario {
+	
+	private long idUsuario;
+	private String nome;
+	private String senha;
+	private String cpf;
+	private String email;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataNascimento;
+
+	public long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 
 }
